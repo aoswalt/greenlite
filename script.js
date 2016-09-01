3fc1f1b0f7ca185dcf8e2093702d94998709decc
Adam Oswalt <email@adamoswalt.com>
2016-09-01 11:30:44 -0500

    code cleanup

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict'
 
 const lights = {
-  a1r: $(".lighta1 .red"),
-  a1y: $(".lighta1 .yellow"),
-  a1g: $(".lighta1 .green"),
-  a1gl: $(".lighta1 .greenLeft"),
-  a2r: $(".lighta2 .red"),
-  a2y: $(".lighta2 .yellow"),
-  a2g: $(".lighta2 .green"),
-  b1r: $(".lightb1 .red"),
-  b1y: $(".lightb1 .yellow"),
-  b1g: $(".lightb1 .green")
-};
+  a1r: $('.lighta1 .red'),
+  a1y: $('.lighta1 .yellow'),
+  a1g: $('.lighta1 .green'),
+  a1gl: $('.lighta1 .greenLeft'),
+  a2r: $('.lighta2 .red'),
+  a2y: $('.lighta2 .yellow'),
+  a2g: $('.lighta2 .green'),
+  b1r: $('.lightb1 .red'),
+  b1y: $('.lightb1 .yellow'),
+  b1g: $('.lightb1 .green')
+}
 
 //NOTE(adam): object to hold bulb lit status
-const lightStatus = {};
-for(const k in lights) { lightStatus[k] = {lit: false}; }
+const lightStatus = {}
+Object.keys(lights).forEach(k => lightStatus[k] = {lit: false} )
 
 const mapping = {
   roadA: {
@@ -37,21 +37,37 @@ const mapping = {
       [lightStatus.b1g]
     ]
   }
-};
+}
+
+//NOTE(adam): order is priority
+const scheduleLabels = ['emergency', 'single', 'repeat', 'day', 'base']
 
 const schedule = {
-  emergency: null,
-  events: null,
-  construction: null,
-  scheduler: null,
   base: {}
-};
+}
 
-//NOTE(adam): order used for priority
-const scheduleLabels = ["emergency", "events", "repeats", "planner", "base"];
+//NOTE(adam): add labels to schedule
+scheduleLabels.forEach(l => schedule[l] = null)
+
+const basePayload = {
+  label: 'base',
+  signals: [{
+    repeat: true,
+    list: [
+      {
+        roadA: {L:0, S:0, R:0},
+        roadB: {L:0, S:1, R:0},
+      },
+      {
+        roadA: {L:0, S:1, R:0},
+        roadB: {L:0, S:0, R:0},
+      },
+    ]
+  }]
+}
 
 const payload = {
-  priority: 2,
+  label: 'repeat',
   signals: [{
     startTime: 0,
     endTime: 0,
@@ -59,75 +75,82 @@ const payload = {
     expireTime: 0,
     list: [
       {
-        roadA: { L: 0, S: 2, R: 0 },
-        roadB: { L: 0, S: 0, R: 0 }
+        roadA: {L:0, S:2, R:0},
+        roadB: {L:0, S:0, R:0}
       },
       {
-        roadA: { L: 2, S: 1, R: 0 },
-        roadB: { L: 0, S: 2, R: 0 }
+        roadA: {L:2, S:1, R:0},
+        roadB: {L:0, S:2, R:0}
       }
     ]
   }]
-};
+}
 
 
 function setLights(signal) {
-  const signalRoads = Object.keys(signal);
+  const signalRoads = Object.keys(signal)
 
   signalRoads.forEach((road) => {
-    const mappingDirections = Object.keys(mapping[road]);
+    const mappingDirections = Object.keys(mapping[road])
     mappingDirections.forEach(dir => {
-      const mapDir = mapping[road][dir];
+      const mapDir = mapping[road][dir]
       //NOTE(adam): toggle all off first
       for(let signalNum = 0; signalNum < mapDir.length; ++signalNum) {
         for(let lightNum = 0; lightNum < mapDir[signalNum].length; ++lightNum) {
-          mapDir[signalNum][lightNum].lit = false;
+          mapDir[signalNum][lightNum].lit = false
         }
       }
-    });
+    })
 
-    const signalDirections = Object.keys(signal[road]);
+    const signalDirections = Object.keys(signal[road])
     signalDirections.forEach(dir => {
       //NOTE(adam): only operate on existing mappingDirections
       if(mapping[road].hasOwnProperty(dir)) {
-        const mapDir = mapping[road][dir];
-        const signalNumber = signal[road][dir];
+        const mapDir = mapping[road][dir]
+        const signalNumber = signal[road][dir]
         for(let statusIndex = 0; statusIndex < mapDir[signalNumber].length; ++statusIndex) {
-          const status = mapDir[signalNumber][statusIndex];
-          status.lit = true;
+          const status = mapDir[signalNumber][statusIndex]
+          status.lit = true
         }
       }
-    });
-  });
+    })
+  })
 
-  for(const k in lightStatus) { lights[k].toggleClass("lit", lightStatus[k].lit); }
+  for(const k in lightStatus) { lights[k].toggleClass('lit', lightStatus[k].lit) }
 }
 
-function pushPayload(level, data) {
-  schedule[level] = data;
+function pushPayload(data) {
+  if(scheduleLabels.indexOf(data.label) > -1) {
+    schedule[data.label] = data
+  } else {
+    console.log("invalid payload label", data.label);
+  }
 }
 
-pushPayload("base", payload);
+pushPayload(basePayload)
+// pushPayload(payload)
 
-let i = 0;
-let activeSignalList = null;
+let i = 0
+let activeSignalList = null
 setInterval(() => {
   //NOTE(adam): order is important
-  scheduleLabels.forEach(label => {
+  // scheduleLabels.forEach(label => {
+  for(const label of scheduleLabels) {
     if(schedule[label]) {
-      //TODO(adam): determine if signal applies now
-      activeSignalList = schedule[label].signals[0].list;
+      //TODO(adam): if signal applies now
+      activeSignalList = schedule[label].signals[0].list
       if(i >= activeSignalList.length && !activeSignalList.repeat) {
-        i = 0;
+        i = 0
       }
+      break
     }
-  });
+  }
 
   //NOTE(adam): fall back to base schedule if nothing applies
   if(!activeSignalList) {
-    activeSignalList = baseSchedule.signals[0].list;
+    activeSignalList = schedule.base.signals[0].list
   }
 
-  setLights(schedule.base.signals[0].list[i++]);
-  if(i >= schedule.base.signals[0].list.length) { i = 0; }
-}, 1000);
+  setLights(activeSignalList[i++])
+  if(i >= activeSignalList.length) { i = 0 }
+}, 1000)
