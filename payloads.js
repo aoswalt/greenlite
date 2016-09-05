6b7209be40d032dafee854c8a24ce59a377f4845
Adam Oswalt <email@adamoswalt.com>
2016-09-04 20:08:12 -0500

    simplify payloads to use priority instead of label

--- payloads.js
+++ payloads.js
@@ -1,38 +1,34 @@
 const basePayload = {
-  label: 'base',
-  signals: [{
-    repeat: true,
-    list: [
-      {
-        roadA: {},
-        roadB: {L:0, S:0, R:0},
-      },
-      {
-        roadA: {L:0, S:0, R:0},
-        roadB: {},
-      },
-    ]
-  }]
+  priority: 0,
+  repeat: true,
+  signals: [
+    {
+      roadA: {},
+      roadB: {L:0, S:0, R:0},
+    },
+    {
+      roadA: {L:0, S:0, R:0},
+      roadB: {},
+    },
+  ]
 }
 
 const payload = {
-  label: 'repeat',
-  signals: [{
-    startTime: 0,
-    endTime: Date.now() + 6000,
-    repeat: true,
-    expireTime: 0,
-    list: [
-      {
-        roadA: {L:0, S:2, R:0},
-        roadB: {L:0, S:0, R:0}
-      },
-      {
-        roadA: {L:2, S:1, R:0},
-        roadB: {L:0, S:2, R:0}
-      }
-    ]
-  }]
+  priority: 5,
+  startTime: 0,
+  endTime: Date.now() + 6000,
+  repeat: true,
+  expireTime: 0,
+  signals: [
+    {
+      roadA: {L:0, S:2, R:0},
+      roadB: {L:0, S:0, R:0}
+    },
+    {
+      roadA: {L:2, S:1, R:0},
+      roadB: {L:0, S:2, R:0}
+    }
+  ]
 }
 
 pushPayload(basePayload)
