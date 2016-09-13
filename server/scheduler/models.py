687c37192a77e5ba97f448dd67053702ece49dfe
Adam Oswalt <email@adamoswalt.com>
2016-09-13 11:27:02 -0500

    add active and timestamp to vertex models

--- server/scheduler/models.py
+++ server/scheduler/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 class Vertex(models.Model):
     label = models.CharField(max_length=75)
     address = models.CharField(max_length=75)
+    active = models.BooleanField(default=True)
+    last_seen = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return '{}: {}'.format(self.id, self.label)
+        return '{}: {} @ {}'.format(self.id, self.label, self.address)
 
 class EventRequest(models.Model):
     requester = models.ForeignKey(User)
