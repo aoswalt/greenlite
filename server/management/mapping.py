a50abee13d03d9566c49695a01bec53d42f9c9a5
Adam Oswalt <email@adamoswalt.com>
2016-09-21 15:17:23 -0500

    add device to scheduler_vertex table on greeting

--- server/management/mapping.py
+++ server/management/mapping.py
@@ -1,4 +1,9 @@
+import os
+import sqlite3
 import time
+
+file_path = os.path.dirname(__file__)
+db_path = os.path.join(file_path, '../db.sqlite3')
 
 device_labels = []
 devices = {label:{
@@ -12,6 +17,15 @@ def greet_device(device_label, address):
         print('WARN: Adding new device - {}'.format(device_label))
         device_labels.append(device_label)
         devices[device_label] = {'label':device_label, 'address':'', 'last_seen':0}
+
+    with sqlite3.connect(db_path) as conn:
+        c = conn.cursor()
+        c.execute( ('INSERT OR REPLACE '
+                    'INTO scheduler_vertex(label, address, active, last_seen) '
+                    'VALUES(?, COALESCE(( '
+                        'SELECT address FROM scheduler_vertex WHERE label=?), '
+                        '?), 1, ?)'),
+                  (device_label, device_label, address, time.time()))
 
     devices[device_label]['address'] = address
     devices[device_label]['last_seen'] = time.time()
