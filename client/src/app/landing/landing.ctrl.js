2d4655399d65fec52928038187a7554120c96cd5
Adam Oswalt <email@adamoswalt.com>
2016-09-22 12:11:05 -0500

    simple navbar

--- client/src/app/landing/landing.ctrl.js
+++ client/src/app/landing/landing.ctrl.js
@@ -1,8 +1,5 @@
 app.controller('landingCtrl', function() {
   const landing = this
 
-  landing.title = 'GreenLite Landing'
-
   setTimeout(() => document.querySelectorAll('#inter1')[0].style.fill = 'red', 2000)
-
 })
